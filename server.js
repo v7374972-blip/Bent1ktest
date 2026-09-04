c3aa19b59a737eb144c8c9b13071698d5f491b40
v7374972-blip <v7374972@gmail.com>
2026-09-04 08:15:20 +0300

    Update server.js

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ function getLoginPage(showError = false) {
 
         .auth-container { 
             background-color: #23252b;
-            padding: 39px 15px 60px; 
+            padding: 39px 15px 70px; 
             border-radius: 6px; 
             width: 100%;
             max-width: 400px;
